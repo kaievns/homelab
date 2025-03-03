0803e572a0b3d829b76f48a21127da940a9ce12e
Kai Evans <hi@kaievans.co>
2025-03-04 10:36:21 +1100

    Throwing in a workable k0s setup

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+ssh kai@192.168.68.1 'bash -s' < infrastructure/k0s-prep.sh
+ssh kai@192.168.68.2 'bash -s' < infrastructure/k0s-prep.sh
+ssh kai@192.168.68.3 'bash -s' < infrastructure/k0s-prep.sh
+
+k0sctl apply --config infrastructure/k0s-config.yaml
+
+k0sctl kubeconfig --config infrastructure/k0s-config.yaml >  ~/.kube/config
