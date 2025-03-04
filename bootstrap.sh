65ae5e820ff6d7e29b491f6f75ce721cccc146ce
Kai Evans <hi@kaievans.co>
2025-03-04 11:01:16 +1100

    Patching the traefik install to work with k0s

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,6 @@ ssh kai@192.168.68.3 'bash -s' < infrastructure/k0s-prep.sh
 k0sctl apply --config infrastructure/k0s-config.yaml
 
 k0sctl kubeconfig --config infrastructure/k0s-config.yaml >  ~/.kube/config
+
+./cluster/matallb/install
+./cluster/traefik/install
